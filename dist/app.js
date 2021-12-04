7924cdf9d1c58c940d2762e92d376ded80093b2b
Carlos Fraile <carlosfrailegomez93@gmail.com>
2021-12-04 12:43:05 +0100

    01-contar ovejas para dormir

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,30 @@
 "use strict";
 console.clear();
-console.log("hola mundo");
+var arrayovejas = [
+    { name: 'Noa', colour: 'azul' },
+    { name: 'Euge', colour: 'rojo' },
+    { name: 'Navidad', colour: 'rojo' },
+    { name: 'Ki Na Ma', colour: 'rojo' }
+];
+var regla = ['A', 'a', 'N', 'n'];
+var main = function () {
+    var nuevoarray = [];
+    arrayovejas.forEach(function (oveja) {
+        if (oveja.colour !== "rojo") {
+            return;
+        }
+        ;
+        var caso = false;
+        regla.forEach(function (letra) { if (oveja.name.indexOf(letra) !== -1) {
+            caso = true;
+        } });
+        if (!caso) {
+            return;
+        }
+        ;
+        nuevoarray.push(oveja);
+    });
+    console.log(nuevoarray);
+};
+main();
 //# sourceMappingURL=app.js.map
