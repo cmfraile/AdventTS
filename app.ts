053998c5265679e42c1da9e71521776caa9efa3b
Carlos Fraile <carlosfrailegomez93@gmail.com>
2021-12-05 10:15:22 +0100

    tenemos que retroceder a copiar el anterior ejercicio

--- app.ts
+++ app.ts
@@ -1,14 +1,13 @@
 console.clear();
 
-let carta = 'bici coche balón _playstation bici coche peluche'.split(' ');
+const carta = "bici coche (balón) bici coche peluche";
+
 const main = () => {
-    const regexp = new RegExp(/[^a-zA-Záéíóú]/g)
-    carta.forEach( (undefined,index) => {if(!!carta[index].match(regexp)){carta.splice(index,1);}});
-    let cartaobjeto:any = {};
-    carta.forEach(obj => {
-        if(cartaobjeto[obj]){ cartaobjeto[obj]++ }else{cartaobjeto[obj] = 1};
-    });
-    console.log(cartaobjeto);
+    const analisis = {
+        inicio : carta.indexOf('('),
+        fin : carta.indexOf(')'),
+        grinched : carta.match()
+    }
 }
 
 main();
