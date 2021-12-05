b677eeb358e0f2d716f2904b4243770b9a8590a3
Carlos Fraile <carlosfrailegomez93@gmail.com>
2021-12-05 12:42:01 +0100

    reset pues no atinamos

--- app.ts
+++ app.ts
@@ -1,13 +1,44 @@
 console.clear();
 
-const carta = "bici coche (balón) bici coche peluche";
+const letter = "(muñeca) consola bici"
+const regla = ['[',']','{','}',' '];
 
-const main = () => {
-    const analisis = {
-        inicio : carta.indexOf('('),
-        fin : carta.indexOf(')'),
-        grinched : carta.match()
-    }
+const grinchck = (letter:string,inicio:number|undefined,fin:number|undefined) => {
+    if(inicio == undefined || fin == undefined){return false} ; if(inicio >= fin){return false} ;
+    let tfletter:any = letter.split('');
+    for(let i = 1 ; i < inicio ; i++){tfletter.shift()};
+    for(let i = (fin + 1) ; i <= (letter.length - 1) ; i++){tfletter.pop()};
+
+    //Compruebo si hay alguno simbolo ilegal entre los parentesis:
+    let caso = true;
+    regla.forEach( x => {
+        if(tfletter.includes(x)){caso = false};
+    });
+    if(!caso){return false};
+    
+    //Compruebo si el parentesis esta vacio:
+    console.log(tfletter)
+    tfletter.forEach((x:string,index:number) => {
+        regla.forEach((y:string) => {
+            if(x = y){tfletter.splice(index,1)}
+        });
+    });
+    console.log(tfletter);
+    
+    tfletter = tfletter.join('');
+    if(tfletter.match(/[^a-zA-Záéíóú]/g)){return true}else{return false};
+    
 }
 
-main();
+const main = (letter:string) => {
+    const analisis:any = {
+        inicio : letter.indexOf('('),
+        fin : letter.indexOf(')'),
+    }
+    analisis.check = grinchck(letter,analisis.inicio,analisis.fin);
+    console.log(analisis);
+    return analisis.check;
+}
+
+//main(letter);
+main(letter)
--- dist/app.js
+++ dist/app.js
@@ -1,22 +1,65 @@
 "use strict";
 console.clear();
-var carta = 'bici coche balón _playstation bici coche peluche'.split(' ');
-var main = function () {
-    var regexp = new RegExp(/[^a-zA-Záéíóú]/g);
-    carta.forEach(function (undefined, index) { if (!!carta[index].match(regexp)) {
-        carta.splice(index, 1);
-    } });
-    var cartaobjeto = {};
-    carta.forEach(function (obj) {
-        if (cartaobjeto[obj]) {
-            cartaobjeto[obj]++;
-        }
-        else {
-            cartaobjeto[obj] = 1;
+var letter = "(muñeca) consola bici";
+var regla = ['[', ']', '{', '}', ' '];
+var grinchck = function (letter, inicio, fin) {
+    if (inicio == undefined || fin == undefined) {
+        return false;
+    }
+    ;
+    if (inicio >= fin) {
+        return false;
+    }
+    ;
+    var tfletter = letter.split('');
+    for (var i = 1; i < inicio; i++) {
+        tfletter.shift();
+    }
+    ;
+    for (var i = (fin + 1); i <= (letter.length - 1); i++) {
+        tfletter.pop();
+    }
+    ;
+    //Compruebo si hay alguno simbolo ilegal entre los parentesis:
+    var caso = true;
+    regla.forEach(function (x) {
+        if (tfletter.includes(x)) {
+            caso = false;
         }
         ;
     });
-    console.log(cartaobjeto);
+    if (!caso) {
+        return false;
+    }
+    ;
+    //Compruebo si el parentesis esta vacio:
+    console.log(tfletter);
+    tfletter.forEach(function (x, index) {
+        regla.forEach(function (y) {
+            if (x = y) {
+                tfletter.splice(index, 1);
+            }
+        });
+    });
+    console.log(tfletter);
+    tfletter = tfletter.join('');
+    if (tfletter.match(/[^a-zA-Záéíóú]/g)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+    ;
 };
-main();
+var main = function (letter) {
+    var analisis = {
+        inicio: letter.indexOf('('),
+        fin: letter.indexOf(')'),
+    };
+    analisis.check = grinchck(letter, analisis.inicio, analisis.fin);
+    console.log(analisis);
+    return analisis.check;
+};
+//main(letter);
+main(letter);
 //# sourceMappingURL=app.js.map
