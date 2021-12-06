a5b5457e8e9143f87bf60b17a3f4712b222218db
Carlos Fraile <carlosfrailegomez93@gmail.com>
2021-12-06 15:35:14 +0100

    checkpoint del arbol de navidad reto 4

--- app.ts
+++ app.ts
@@ -2,15 +2,40 @@ console.clear();
 
 const main = (altura:number) => {
     //let valor = Math.ceil((altura / 2));
-    let cadena:string = "";
+    let cadena = "";
+    const generarancho = () => {
+        let caso:string = "";
+        for(let i = 1 ; i <= altura ; i++){caso = caso + "_"};
+        return caso
+    }
+    let ancho:string = generarancho();
 
+    /*
     for(let i = 1 ; i <= altura ; i++ ){
+        let piso = "";
         for( let y = 0 ; y <= altura -1 ; y++ ){
-            cadena = cadena + "_";
+            piso = piso + "*" ;
         }
+        cadena = ancho + piso + ancho ;
         cadena = cadena + "\n";
     }
+    */
 
+    for(let i = 1 ; i <= altura ; i++){
+        let piso:any = ancho;
+        if(i == 1){
+            piso = piso + "*" + ancho;
+            while(piso.length !== altura){ piso = piso.split('') ; piso.shift() ; piso.pop() ; piso = piso.join('')};
+            cadena = piso + "\n" + cadena;
+            piso = ancho;
+        }
+        for(let y = 1 ; y <= altura ; y++){
+
+        };
+    }
+
+    
+    //tronco del arbol de navidad:
     for(let i = 1 ; i <= 2 ; i++){
         for( let y = 1 ; y <= altura ; y++ ){
             if(y == (Math.ceil(altura / 2)) ){ cadena = cadena + "#" }else{ cadena = cadena + "_" }
--- dist/app.js
+++ dist/app.js
@@ -3,12 +3,44 @@ console.clear();
 var main = function (altura) {
     //let valor = Math.ceil((altura / 2));
     var cadena = "";
-    for (var i = 1; i <= altura; i++) {
-        for (var y = 0; y <= altura - 1; y++) {
-            cadena = cadena + "_";
+    var generarancho = function () {
+        var caso = "";
+        for (var i = 1; i <= altura; i++) {
+            caso = caso + "_";
         }
+        ;
+        return caso;
+    };
+    var ancho = generarancho();
+    /*
+    for(let i = 1 ; i <= altura ; i++ ){
+        let piso = "";
+        for( let y = 0 ; y <= altura -1 ; y++ ){
+            piso = piso + "*" ;
+        }
+        cadena = ancho + piso + ancho ;
         cadena = cadena + "\n";
     }
+    */
+    for (var i = 1; i <= altura; i++) {
+        var piso = ancho;
+        if (i == 1) {
+            piso = piso + "*" + ancho;
+            while (piso.length !== altura) {
+                piso = piso.split('');
+                piso.shift();
+                piso.pop();
+                piso = piso.join('');
+            }
+            ;
+            cadena = piso + "\n" + cadena;
+            piso = ancho;
+        }
+        for (var y = 1; y <= altura; y++) {
+        }
+        ;
+    }
+    //tronco del arbol de navidad:
     for (var i = 1; i <= 2; i++) {
         for (var y = 1; y <= altura; y++) {
             if (y == (Math.ceil(altura / 2))) {
