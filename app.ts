289af4ec84fda36217380a02aa22b493d0db575f
Carlos Fraile <carlosfrailegomez93@gmail.com>
2021-12-06 17:00:15 +0100

    vamos a otro reto de JS, pues estamos atrancados y tilteados con el arbol de navidad

--- app.ts
+++ app.ts
@@ -1,42 +1,15 @@
 console.clear();
 
 const main = (altura:number) => {
-    //let valor = Math.ceil((altura / 2));
     let cadena = "";
-    const generarancho = () => {
-        let caso:string = "";
-        for(let i = 1 ; i <= altura ; i++){caso = caso + "_"};
-        return caso
-    }
-    let ancho:string = generarancho();
-
-    //hojas arbol de navidad:
+    let hojas = 1;
+    let ancho = (altura * 2) - 1;
     for(let i = 1 ; i <= altura ; i++){
-        if(i == 1){
-            let piso:any = ancho;
-            piso = piso + "*" + ancho;
-            while(piso.length !== altura){ piso = piso.split('') ; piso.shift() ; piso.pop() ; piso = piso.join('')};
-            cadena = piso + "\n" + cadena;
-            piso = ancho;
-        }else{
-            let piso:any = ancho;
-            let contador = 1;
-            for(let y = 1 ; y <= (contador + 2) ; y++){piso = piso + "*" };
-            piso = piso + ancho;
-            cadena = cadena + piso + "\n" ;
+        let piso = "";
+        for(let y = 1 ; y <= ancho ; y++){
+            if(y <)
         }
     }
-
-    
-    //tronco del arbol de navidad:
-    for(let i = 1 ; i <= 2 ; i++){
-        for( let y = 1 ; y <= altura ; y++ ){
-            if(y == (Math.ceil(altura / 2)) ){ cadena = cadena + "#" }else{ cadena = cadena + "_" }
-        }
-        cadena = cadena + "\n";
-    }
-
-    console.log(cadena);
 }
 
 main(5);
--- dist/app.js
+++ dist/app.js
@@ -1,57 +1,16 @@
 "use strict";
 console.clear();
 var main = function (altura) {
-    //let valor = Math.ceil((altura / 2));
     var cadena = "";
-    var generarancho = function () {
-        var caso = "";
-        for (var i = 1; i <= altura; i++) {
-            caso = caso + "_";
-        }
-        ;
-        return caso;
-    };
-    var ancho = generarancho();
-    //hojas arbol de navidad:
+    var hojas = 1;
+    var ancho = (altura * 2) - 1;
     for (var i = 1; i <= altura; i++) {
-        if (i == 1) {
-            var piso = ancho;
-            piso = piso + "*" + ancho;
-            while (piso.length !== altura) {
-                piso = piso.split('');
-                piso.shift();
-                piso.pop();
-                piso = piso.join('');
-            }
-            ;
-            cadena = piso + "\n" + cadena;
-            piso = ancho;
-        }
-        else {
-            var piso = ancho;
-            var contador = 1;
-            for (var y = 1; y <= (contador + 2); y++) {
-                piso = piso + "*";
-                piso = piso + ancho;
-                cadena = cadena + piso + "\n";
-                piso = ancho;
-            }
-            ;
+        var piso = "";
+        for (var y = 1; y <= ancho; y++) {
+            if (y < )
+                ;
         }
     }
-    //tronco del arbol de navidad:
-    for (var i = 1; i <= 2; i++) {
-        for (var y = 1; y <= altura; y++) {
-            if (y == (Math.ceil(altura / 2))) {
-                cadena = cadena + "#";
-            }
-            else {
-                cadena = cadena + "_";
-            }
-        }
-        cadena = cadena + "\n";
-    }
-    console.log(cadena);
 };
 main(5);
 //# sourceMappingURL=app.js.map
