58cf49f8c1d6091c30a9e9dbbfbbdf1ab200ec1b
Carlos Fraile <carlosfrailegomez93@gmail.com>
2021-12-08 15:53:36 +0100

    checkpoint para el reto 6

--- app.ts
+++ app.ts
@@ -1,7 +1,18 @@
 console.clear();
 
-const main = () => {
+const paresobj = {notas: [6, 7, 1, 2],objetivo: 8};
 
+const main = (notasarr:number[],obj:number) => {
+    let caso:any = [];
+    notasarr.forEach((x,index1) => {
+        notasarr.forEach((y,index2) => {
+            if(index1 !== index2){
+                if(x + y == obj){caso = [x,y]}
+            }
+        });
+    });
+    if (caso.length == 0){caso = null};
+    console.log(caso);
 }
 
-main();
+main(paresobj.notas,paresobj.objetivo);
--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,22 @@
 "use strict";
 console.clear();
-var main = function () {
+var paresobj = { notas: [6, 7, 1, 2], objetivo: 8 };
+var main = function (notasarr, obj) {
+    var caso = [];
+    notasarr.forEach(function (x, index1) {
+        notasarr.forEach(function (y, index2) {
+            if (index1 !== index2) {
+                if (x + y == obj) {
+                    caso = [x, y];
+                }
+            }
+        });
+    });
+    if (caso.length == 0) {
+        caso = null;
+    }
+    ;
+    console.log(caso);
 };
-main();
+main(paresobj.notas, paresobj.objetivo);
 //# sourceMappingURL=app.js.map
