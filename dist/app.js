02b334b428c0dc9e40773b025393cdaeddbeff53
Carlos Fraile <carlosfrailegomez93@gmail.com>
2021-12-11 18:16:57 +0100

    realizada la practica de los cambios

--- dist/app.js
+++ dist/app.js
@@ -1,36 +1,30 @@
 "use strict";
 console.clear();
-<<<<<<< HEAD
-var pricesBtc = [39, 18, 29, 25, 34, 32, 5];
-var pricesDoge = [18, 15, 12, 11, 9, 7];
-var main = function (prices) {
-    var criterio = { compra: null, vende: null, beneficio: -1 };
-    for (var x = 0; x <= prices.length; x++) {
-        for (var y = 0; y <= prices.length; y++) {
-            if (x !== y && x <= y) {
-                if ((prices[y] - prices[x]) > criterio.beneficio) {
-                    criterio.compra = prices[x];
-                    criterio.vende = prices[y];
-                    criterio.beneficio = prices[y] - prices[x];
-                }
-            }
-            ;
+var coins = [50, 20, 10, 5, 2, 1];
+var main = function (change) {
+    var contado = 0;
+    var indice = 0;
+    var cambiobj = { 50: 0, 20: 0, 10: 0, 5: 0, 2: 0, 1: 0 };
+    while (contado < change) {
+        if ((contado + coins[indice]) <= change) {
+            contado += coins[indice];
+            cambiobj["".concat(coins[indice])]++;
+        }
+        else {
+            indice++;
         }
     }
-    return criterio.beneficio;
-=======
-var main = function (altura) {
-    var cadena = "";
-    var hojas = 1;
-    var ancho = (altura * 2) - 1;
-    for (var i = 1; i <= altura; i++) {
-        var piso = "";
-        for (var y = 1; y <= ancho; y++) {
-            if (y < )
-                ;
-        }
-    }
->>>>>>> master
+    return [cambiobj[1], cambiobj[2], cambiobj[5], cambiobj[10], cambiobj[20], cambiobj[50]];
 };
-main(pricesDoge);
+console.log(main(14));
+/*while(contado < change){
+  let indice = 0;
+  console.log(indice);
+  if(contado + coins[indice] < change){
+    change = change + coins[indice];
+    cambiobj[`${coins[indice]}c`]++;
+  } else {
+    indice++
+  }
+}*/
 //# sourceMappingURL=app.js.map
