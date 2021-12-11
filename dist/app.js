45eac2adc8aa798a94cd98225f8d0c1046d4bb8a
Carlos Fraile <carlosfrailegomez93@gmail.com>
2021-12-11 16:53:47 +0100

    terminamos el adviento de criptomonedas

--- dist/app.js
+++ dist/app.js
@@ -1,47 +1,22 @@
 "use strict";
 console.clear();
-var store1 = {
-    'estanteria1': {
-        'cajon1': {
-            'producto1': 'coca-cola',
-            'producto2': 'fanta',
-            'producto3': 'sprite'
-        }
-    },
-    'estanteria2': {
-        'cajon1': 'vacio',
-        'cajon2': {
-            'producto1': 'pantalones',
-            'producto2': 'camiseta' // <- ¡Está aquí!
-        }
-    }
-};
-var store2 = {
-    'baul': {
-        'fondo': {
-            'objeto': 'cd-rom',
-            'otro-objeto': 'disquette',
-            'otra-cosa': 'mando'
-        }
-    }
-};
-var main = function (store, product) {
-    var objetos = [];
-    for (var x in store) {
-        for (var y in store[x]) {
-            for (var z in store[x][y]) {
-                if (store[x][y][z].length !== 1) {
-                    objetos.push(store[x][y][z]);
+var pricesBtc = [39, 18, 29, 25, 34, 32, 5];
+var pricesDoge = [18, 15, 12, 11, 9, 7];
+var main = function (prices) {
+    var criterio = { compra: null, vende: null, beneficio: -1 };
+    for (var x = 0; x <= prices.length; x++) {
+        for (var y = 0; y <= prices.length; y++) {
+            if (x !== y && x <= y) {
+                if ((prices[y] - prices[x]) > criterio.beneficio) {
+                    criterio.compra = prices[x];
+                    criterio.vende = prices[y];
+                    criterio.beneficio = prices[y] - prices[x];
                 }
             }
+            ;
         }
     }
-    if (objetos.indexOf(product) !== -1) {
-        return true;
-    }
-    else {
-        return false;
-    }
+    return criterio.beneficio;
 };
-console.log(main(store1, 'sprite'));
+main(pricesDoge);
 //# sourceMappingURL=app.js.map
