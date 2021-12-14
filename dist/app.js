a9fbf48725309e98e4dc666f93048a4ff282329e
Carlos Fraile <carlosfrailegomez93@gmail.com>
2021-12-14 19:02:18 +0100

    el de los renos esta realizado, pero no pasa los test

--- dist/app.js
+++ dist/app.js
@@ -1,29 +1,14 @@
 "use strict";
 console.clear();
-var obs = [14, 10, 8, 2, 3, 6];
-var main = function (obstacles) {
-    obstacles = obstacles.sort(function (a, b) { return a - b; });
-    var _loop_1 = function (i) {
-        var saltos = [];
-        for (var y = 0; y <= obstacles[obstacles.length - 1]; y++) {
-            if (y % i == 0) {
-                saltos.push(y);
-            }
-            ;
-            saltos.forEach(function (x, index) { if (obstacles.includes(x)) {
-                saltos[index] = "X";
-            } });
+var ids = [5, 6, 1, 2, 3, 7, 0].sort();
+var main = function (ids) {
+    for (var i = 0; i <= ids[ids.length - 1]; i++) {
+        if (i !== ids[i]) {
+            return i;
         }
-        if (!saltos.includes("X")) {
-            return { value: saltos[1] };
-        }
-        ;
-    };
-    for (var i = 1; i <= obstacles[obstacles.length - 1]; i++) {
-        var state_1 = _loop_1(i);
-        if (typeof state_1 === "object")
-            return state_1.value;
     }
+    ;
+    return ids.length + 1;
 };
-console.log(main(obs));
+console.log(main(ids));
 //# sourceMappingURL=app.js.map
